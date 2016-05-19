792cbfda16a71521dfdf1afff879f99029c2b405
Garrett <garrett.smestad@gmail.com>
2016-05-19 04:20:22 -0600

    Fixed error in round.rb file

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -11,14 +11,14 @@ cards = CardGenerator.new(filename).cards
 deck = Deck.new(cards)
 
 round = Round.new(deck)
-
-round.start
+#
+# round.start
 
 puts "Welcome! You're playing with #{deck.count} cards."
 puts "---------------------------------------------------"
 
 round.deck.cards.each do |card|
-  puts "This is card number #{round.guesses.count + 1} out of #{round.deck.count}."  
+  puts "This is card number #{round.guesses.count + 1} out of #{round.deck.count}."
   puts "Question: #{card.question}"
   user_input = gets.chomp.to_s
   round.record_guess(user_input)
